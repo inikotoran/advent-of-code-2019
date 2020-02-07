685c740d489c0a39634bb23dae35f3797a53514f
niko.juliyanto <niko.juliyanto@go-jek.com>
2020-02-07 17:35:49 +0700

    use printf

--- day1/main.clj
+++ day1/main.clj
@@ -6,4 +6,4 @@
 (with-open [rdr (clojure.java.io/reader "day1/input.txt")] 
     (doseq [line (line-seq rdr)] 
         (def result (atom (+ @result (calculate_fuel_requirement (Integer. line)))))))
-(print (format "%d\n" @result))
+(printf "%d\n" @result)
